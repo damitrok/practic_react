20fca19ab72d2ef0edd606e7b9214eadc9820fd5
damitrok <mitrofan12a@gmail.com>
2022-08-04 22:04:51 +0300

    create posts app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState } from 'react';
+import Counter from './component/counter';
+import PostForm from './component/postForm';
+import PostItem from './component/postItem';
+import PostList from './component/postList';
+import MySelect from './component/UI/select/MySelect';
+import './styles/app.scss'
 
 function App() {
+  
+  const [posts, setPosts] = useState([
+    {id: '1', title: 'javaScript', body: 'бб'},
+    {id: '2', title: 'аа', body: 'аа'},
+    {id: '3', title: 'бб' , body: 'гг'},
+    {id: '4', title: 'вв', body: 'яя'}
+  ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const createPost = (newPost) =>{
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) =>{
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <PostForm create={createPost}/>
+      <hr style={{margin:'15px 0'}}/>
+      <div> 
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue = "Сортировка по"
+          options={[
+            {value: 'title', name: 'По заголовку'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
+      {posts.length 
+        ? <PostList remove={removePost} posts={posts} title="Посты про javaScript"/>
+        : <h1 style={{textAlign: ' center'}}>Посты не были найдены!</h1>
+      }
+      
+    </div> 
   );
 }
 
---
+++ src/component/UI/input/MyInput.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MyInput.module.scss";
+
+const MyInput =(props) => {
+  return <input  {...props} className={classes.myInp} />;
+};
+
+export default MyInput;
