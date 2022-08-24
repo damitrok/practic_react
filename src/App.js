16dcb38ca923fb2dfdf6f2370c5df7b92ca3b21d
damitrok <mitrofan12a@gmail.com>
2022-08-24 18:22:52 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {  useEffect, useMemo, useState } from 'react';
+import PostService from './API/postService';
 import Counter from './component/counter';
 import PostFilter from './component/PostFilter';
 import PostForm from './component/postForm';
@@ -7,38 +8,49 @@ import PostItem from './component/postItem';
 import PostList from './component/postList';
 import MyButton from './component/UI/button/MyButton';
 import MyInput from './component/UI/input/MyInput';
+import Loader from './component/UI/loader/Loader';
 import MyModal from './component/UI/MyModal/MyModal';
+import Paginations from './component/UI/paginations/Paginations';
 import MySelect from './component/UI/select/MySelect';
+import { useFetch } from './hooks/useFetch';
 import { usePost } from './hooks/usePost';
 import './styles/app.scss'
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
-  
   const [posts, setPosts] = useState([]);
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const [filter, setFilter] = useState({sort:'', query:''});
   const sortedAndSearchedPosts = usePost(posts, filter.sort, filter.query);
   
 
+  const [fetchPosts, isPostsLoading, postError] = useFetch(async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
+  })
+
   useEffect(() => {
-    fetchPosts()
-  },[])
+    fetchPosts(limit, page)
+  },[page])
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
-  }
-
   const removePost = (post) =>{
     setPosts(posts.filter(p => p.id !== post.id))
   };
 
-  
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit,page)
+  }
 
   return (
     <div className="App">
@@ -50,7 +62,14 @@ function App() {
       </MyModal>
       <hr style={{margin:'15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про javaScript"/>
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{display:'flex', justifyContent:'center', marginTop:50}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про javaScript"/>
+      }
+      <Paginations page={page} changePage={changePage} totalPages={totalPages}/>
     </div> 
   );
 }
